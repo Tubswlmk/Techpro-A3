f0c2daee44e47be24af3f5a97de5142e81677bea
Toby <tobylclaveria@gmail.com>
2026-01-30 15:36:49 +0800

    Update Seatwork1.java

--- Quarter3-folder/Seatwork1.java
+++ Quarter3-folder/Seatwork1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Seatwork1 {
+public class Seatwork1Q3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
      
